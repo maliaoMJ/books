a8f6ff49ebb9fef8fb2f38532ebcf28e716d86a1
majian <jian.ma@lavector.com>
2018-09-11 15:03:51 +0800

    添加布局layout文件

---
+++ src/layout/default.vue
@@ -0,0 +1,26 @@
+<template>
+	<div class="defaultLayout">
+    <solt name="container"></solt> 
+  </div>
+</template>
+
+<script type="text/javascript">
+export default {
+  name: "DefaultLayout",
+  props: {},
+  data() {
+    return {};
+  },
+  mounted() {},
+  methods: {}
+};
+</script>
+<style lang="scss">
+.defaultLayout{
+	width:100%;
+	height: 100%；
+	position:relative;
+	display:flex;
+	flex-direction: row;
+}
+</style>
